6a6f324c49f17abef0c0d58d86d0aa37919e6bcc
Stanislas Bagnol <stan.bagnol@gmail.com>
2020-12-10 16:39:07 +0100

    recup parametre dans tableau

--- controleurs/controleurGenerer.php
+++ controleurs/controleurGenerer.php
@@ -1,13 +1,55 @@
 <?php
 $message ="";
+$environnements = getAttribut("Environnement", "nomEnvironnement");
+
+
+function getParametreCarte(){
+	$param = array(
+		'nomCarte'        => $_POST['nomCarte'],
+		'nom'	          => $_POST['nomContrib'],
+		'prenom'          => $_POST['prenomContrib'],
+		'description'     => $_POST['descriptionCarte'],
+		'nbMoyenZone'     => $_POST['nbMoyenzone'],
+		'nbPassageSecret' => $_POST['nbPassageSecret'], 
+		'objectif'        => $_POST['listObjectif'],
+
+  
+		'zone'            => array('min' => $_POST['minZone'], 'max' => $_POST['maxZone']),
+
+		'dimZone'         => array('min' => $_POST['dimMinZone'], 'max' => $_POST['dimMaxZone']),
+
+		'mobilier'        => array('min' => $_POST['nbMobilierMin'], 'max' => $_POST['nbMobilierMax']),
+
+		'mobilier'        => array('min' => $_POST['nbMobilierMin'], 'max' => $_POST['nbMobilierMax']),
+
+		'piege'           => array('min' => $_POST['nbPiegeMin'], 'max' => $_POST['nbPiegeMax']),
+
+		'equipement'      => array('min' => $_POST['nbEquipementMin'], 'max' => $_POST['nbEquipementMax']),
+
+		'creature'        => array('min' => $_POST['nbCreatureMin'], 'max' => $_POST['nbCreatureMax']),
+
+		'pnj'             => array('min' => $_POST['nbPnjMin'], 'max' => $_POST['nbPnjMax'])
+
+
+
+	);
+
+	return $param;
+}
+
+
 
 
 if(isset($_POST['boutonGenerer'])){
-	$nomCarte = $_POST['nomCarte'];
-	$prenom   = $_POST['prenomContrib'];
-	$nom      =  $_POST['nomContrib'];
-	initCarte($nomCarte, "description");
-	createContrib($nom, $prenom, $nomCarte);
+
+	$param = getParametreCarte();
+
+	$message = initCarte($param['nomCarte'], $param['description']);
+	createContrib($param['nom'], $param['prenom'], $param['nomCarte']);
+	
+
+
+
 }
 
 
